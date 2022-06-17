4b4df8e037ba73f959bade570bddd4c35261c8ca
Ramil Saavedra <ramilsmcl@gmail.com>
2022-06-17 10:47:53 +0800

    show loading, data and error in ui

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
+
 const RepositoriesList: React.FC = () => {
+  const { searchRepositories } = useActions();
+  const [term, setTerm] = useState('');
+  const { data, loading, error } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error &&
+        !loading &&
+        data.map((name, index) => {
+          return <p key={index}>{name}</p>;
+        })}
     </div>
   );
 };
