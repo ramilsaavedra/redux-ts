58b9247ce2934f70a4ee1b561ed78684692981e7
Ramil Saavedra <ramilsmcl@gmail.com>
2022-06-17 09:33:04 +0800

    app components init

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import RepositoriesList from './RepositoriesList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Search For a Package</h1>
+      <RepositoriesList />
+    </div>
+  );
+};
+
+export default App;
